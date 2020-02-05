b069f53d58a08317b670ed43f47165cf65ec04fa
Agustinus Adam <agustinusadamw@gmail.com>
2020-02-05 22:39:30 +0700

    Update code file hitung-jumlah-kata.js

--- hitung-jumlah-kata.js
+++ hitung-jumlah-kata.js
@@ -1,12 +1,17 @@
 // Menghitung Jumlah Kata
 function hitungJumlahKata(kalimat) {
     // you can only write your code here!
-    var kalimatArr = kalimat.split(' ')
-    return kalimatArr.length
-    
+    var jumlah = 0;
+    for (i = 0; i < kalimat.length; i++) {
+      if (kalimat[i] == ' ') {
+        jumlah++
+      }
+    }
+    jumlah += 1
+    return jumlah
 }
   
-  // TEST CASES
+// TEST CASES
 console.log(hitungJumlahKata('I have a dream')); // 4
 console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
 console.log(hitungJumlahKata('A song to sing')); // 4
