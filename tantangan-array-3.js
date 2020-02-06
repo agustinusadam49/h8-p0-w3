3d57bc8cecade9781e2996de225146c056b050ba
Agustinus Adam <agustinusadamw@gmail.com>
2020-02-06 11:36:14 +0700

    Update file tantangan-array-3.js

--- tantangan-array-3.js
+++ tantangan-array-3.js
@@ -66,4 +66,12 @@ function dataHandling2(masukan) {
 var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
 dataHandling2(input);
 
-
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah
+ */
